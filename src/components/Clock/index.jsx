ce34d82ac4d5071cc505eb216cddacbb7ffb8f86
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-10-10 02:11:41 +0800

    init

---
+++ src/components/Clock/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss'
+export default function Clock(){
+
+    return(
+        <>
+            test
+            <div className={'test'}>
+                asdasdasdasdasd
+
+            </div>
+        </>
+    )
+}
